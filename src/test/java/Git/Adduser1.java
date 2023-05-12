3e243d6d86124e829d94cff19554e4b92af0b760
Venkey0467 <s.venkataramana0353@gmail.com>
2023-05-12 06:32:38 -0700

    deeeee

---
+++ src/test/java/Git/Adduser1.java
@@ -0,0 +1,75 @@
+package Git;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class Adduser1 {
+	
+	@Test
+	public void Post1 ()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"boss\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+			.when()
+				.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+	
+	@Test
+	public void Post2 ()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"boss\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+			.when()
+				.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+	
+	@Test
+	public void Post3 ()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"boss\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+			.when()
+				.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+	
+	@Test
+	public void Post4 ()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"boss\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+			.when()
+				.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+}
