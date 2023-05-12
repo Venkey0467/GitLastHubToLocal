770ad616e4c9ea7736f2cb55a548a39a5d00afeb
Venkey0467 <s.venkataramana0353@gmail.com>
2023-05-12 06:25:04 -0700

    ddd

--- src/test/java/Git/Adduser.java
+++ src/test/java/Git/Adduser.java
@@ -6,7 +6,24 @@ import static io.restassured.RestAssured.*;
 public class Adduser {
 	
 	@Test
-	public void Post ()
+	public void Post1 ()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"boss\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+			.when()
+				.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
+	
+	@Test
+	public void Post2 ()
 	{
 		given()
 			.contentType("application/json")
