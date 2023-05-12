d7217eb50d42477e6338019619b694bdadb54adc
Venkey0467 <s.venkataramana0353@gmail.com>
2023-05-12 06:30:31 -0700

    deeeee

--- src/test/java/Git/Adduser.java
+++ src/test/java/Git/Adduser.java
@@ -55,4 +55,21 @@ public class Adduser {
 		.then()
 			.log().all();
 	}
+	
+	@Test
+	public void Post4 ()
+	{
+		given()
+			.contentType("application/json")
+			.body("{\r\n"
+					+ "    \"name\": \"boss\",\r\n"
+					+ "    \"role\": \"qa\",\r\n"
+					+ "    \"age\": 31,\r\n"
+					+ "    \"city\": \"hyd\"\r\n"
+					+ "}")
+			.when()
+				.post("http://localhost:3000/posts/")
+		.then()
+			.log().all();
+	}
 }
